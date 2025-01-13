bbbb47639b17699feb31ab05082c99495159bdd5
nicou11 <lyoungin1204@gmail.com>
2025-01-13 22:38:51 +0900

    다시 풀어보기

---
+++ Programmers/Lv.0/문자열 묶기.py
@@ -0,0 +1,7 @@
+strArr = ["a","bc","d","efg","hi"]
+
+def solution(strArr):
+    answer = {}
+    for i in strArr:
+        answer[len(i)] = answer.get(len(i),0) + 1
+    return max(answer.values())
