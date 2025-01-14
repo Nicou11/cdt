62b6b72c16953aa0e42ee5ad5701340fe0c804db
nicou11 <lyoungin1204@gmail.com>
2025-01-14 21:36:23 +0900

    인덱스

---
+++ Programmers/Lv.0/2의 영역.py
@@ -0,0 +1,16 @@
+arr = [1,2,1,2,1,10,2,1]
+
+def solution(arr):
+    if 2 not in arr:
+        return [-1]
+    first2 = arr.index(2)
+    last2 = len(arr) - 1 - arr[::-1].index(2)
+    return arr[first2:last2 + 1]
+
+
+def solution2(arr):
+    if 2 not in arr:
+        return [-1]
+    index2 = [i for i,n in enumerate(arr) if n == 2]
+    result = arr[index2[0]:index2[-1]+1]
+    return result
