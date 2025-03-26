c818b40147d5259e4938842cdf68f00ab222315b
nicou11 <lyoungin1204@gmail.com>
2025-03-27 08:09:17 +0900

    조건과 인덱싱

---
+++ Programmers/Lv.0/짝수 홀수 개수.py
@@ -0,0 +1,17 @@
+def solution(num_list):
+    e = 0
+    o = 0
+    for i in num_list:
+        if i%2 == 0:
+            e += 1
+        else:
+            o += 1
+    return [e,o]
+
+#다른 풀이
+def solution(num_list):
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer
+#인덱싱을 이용하여 개수 count
